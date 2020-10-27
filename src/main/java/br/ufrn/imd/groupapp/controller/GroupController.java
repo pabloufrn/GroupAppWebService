adf5a42487be8cb64071dd2a067058d7cc631f86
rafaelnribeiro <rafaelnribeiro@outlook.com>
2020-10-27 18:03:13 -0300

    Adding ResponseEntity and changing user table name

--- src/main/java/br/ufrn/imd/groupapp/controller/GroupController.java
+++ src/main/java/br/ufrn/imd/groupapp/controller/GroupController.java
@@ -4,31 +4,34 @@ import br.ufrn.imd.groupapp.error.GroupNotFoundException;
 import br.ufrn.imd.groupapp.model.Group;
 import br.ufrn.imd.groupapp.repository.GroupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
 public class GroupController {
 
     @Autowired
     private GroupRepository repository;
 
     @GetMapping("/group")
-    List<Group> findAll() {
-        return repository.findAll();
+    ResponseEntity<List<Group>> findAll() {
+        List<Group> groups = repository.findAll();
+        return ResponseEntity.ok(groups);
     }
 
     @PostMapping("/group")
-    @ResponseStatus(HttpStatus.CREATED)
-    Group newGroup(@RequestBody Group newGroup) {
-        return repository.save(newGroup);
+    ResponseEntity<Group> newGroup(@RequestBody Group newGroup) {
+        Group group = repository.save(newGroup);
+        return ResponseEntity.ok(group);
     }
 
     @GetMapping("/group/{id}")
-    Group findOne(@PathVariable Long id) {
-        return repository.findById(id)
+    ResponseEntity<Group> findOne(@PathVariable Long id) {
+        Group group = repository.findById(id)
                 .orElseThrow(() -> new GroupNotFoundException(id));
+        return ResponseEntity.ok(group);
     }
 
 }
--- src/main/java/br/ufrn/imd/groupapp/model/User.java
+++ src/main/java/br/ufrn/imd/groupapp/model/User.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
 
 @Data
 @Entity
+@Table(name = "user_tb")
 public class User {
     private String name;
 
